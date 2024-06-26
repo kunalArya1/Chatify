b6f39c4756e1ddc041176a6282f93637d254d29c
kunalArya1 <kunalkrraj220@gmail.com>
2024-06-26 10:24:21 +0530

    Chat Model Created

---
+++ backend/model/chat.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.model(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("chat", chatSchema);
